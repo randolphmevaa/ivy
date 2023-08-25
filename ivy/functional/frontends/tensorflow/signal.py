f7aae17dc7c85af9f56bfacf543300e3ad8fddbb
Randolph MEVA'A <rmevaa@gmail.com>
2023-08-25 09:34:59 -0400

    Add linear_to_mel_weight_matrix to signal

--- ivy/functional/frontends/tensorflow/signal.py
+++ ivy/functional/frontends/tensorflow/signal.py
@@ -19,3 +19,16 @@ kaiser_window.supported_dtypes = ("float32", "float64", "float16", "bfloat16")
 def idct(input, type=2, n=None, axis=-1, norm=None, name=None):
     inverse_type = {1: 1, 2: 3, 3: 2, 4: 4}[type]
     return ivy.dct(input, type=inverse_type, n=n, axis=axis, norm=norm)
+
+
+# linear_to_mel_weight_matrix
+def linear_to_mel_weight_matrix(
+        num_mel_bins=20,
+        num_spectrogram_bins=129,
+        sample_rate=8000,
+        lower_edge_hertz=125.0,
+        upper_edge_hertz=3800.0,
+        dtype=tf.dtypes.float32,
+        name=None
+):
+
